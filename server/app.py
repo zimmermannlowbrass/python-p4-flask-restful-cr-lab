bbc93245d428e0415904d81742266be064289f70
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-31 09:19:24 -0500

    completed labwork

--- server/app.py
+++ server/app.py
@@ -17,10 +17,51 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    def get(self):
+        plants = [n.to_dict() for n in Plant.query.all()]
+        
+        response = make_response(
+            plants,
+            200
+        )
+        return response
+    
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name = data['name'],
+            image = data['image'],
+            price = data['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        new_plant_dict = new_plant.to_dict()
+
+        response = make_response(
+            new_plant_dict,
+            201,
+        )
+
+        return response
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id==id).first().to_dict()
+
+        response = make_response(
+            plant,
+            200
+        )
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
